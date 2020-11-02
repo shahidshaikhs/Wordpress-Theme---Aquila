927fb1a7284c7ef2c6932f9b346a8cfc11aa173b
MEDIA\shahid.sh <shahid.sh@media.net>
2020-11-02 13:03:24 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Summary (no period for file headers)
+ *
+ * Description. (use period)
+ *
+ * @link https://teamtreehouse.com/community/wordpress-php-package-etc-question
+ *
+ * @package Aquila
+ * @subpackage Component
+ * @since 2nd Nov, 2020
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
